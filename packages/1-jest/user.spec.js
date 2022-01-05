b7ed33de7f3ed6a36190a9f1cffce888727739bf
adrianpdm <adrian.padmodihardjo@gmail.com>
2022-01-06 02:13:58 +0700

    test: add empty test suite for "User" class

---
+++ packages/1-jest/user.spec.js
@@ -0,0 +1,15 @@
+const { User } = require('./user')
+
+describe('Entity: User', () => {
+  it('correctly set name and age via ctor', () => {
+    
+  })
+
+  it('correctly update name', () => {
+    
+  })
+
+  it('correctly update age', () => {
+    
+  })
+})
