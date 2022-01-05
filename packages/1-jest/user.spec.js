7b36b3eab0af86063274bdc584a8797079f90ebf
adrianpdm <adrian.padmodihardjo@gmail.com>
2022-01-06 02:14:47 +0700

    test: create test script for each "User" class fn

--- packages/1-jest/user.spec.js
+++ packages/1-jest/user.spec.js
@@ -2,14 +2,29 @@ const { User } = require('./user')
 
 describe('Entity: User', () => {
   it('correctly set name and age via ctor', () => {
-    
+    const user = new User({
+      name: 'adrian',
+      age: 17,
+    })
+    expect(user.name).toBe('adrian')
+    expect(user.age).toBe(17)
   })
 
   it('correctly update name', () => {
-    
+    const user = new User({
+      name: 'adrian',
+      age: 17,
+    })
+    user.setName('padmodihardjo')
+    expect(user.name).toBe('padmodihardjo')
   })
 
   it('correctly update age', () => {
-    
+    const user = new User({
+      name: 'adrian',
+      age: 17,
+    })
+    user.setAge(30)
+    expect(user.age).toBe(30)
   })
 })
