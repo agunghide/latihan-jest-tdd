f222805b9bd7869b14d8ab6244df92364603f1ea
adrianpdm <adrian.padmodihardjo@gmail.com>
2022-01-06 03:12:09 +0700

    test: add "beforeEach" to remove duplicated ctors

--- packages/1-jest/user.spec.js
+++ packages/1-jest/user.spec.js
@@ -1,31 +1,28 @@
 const { User } = require('./user')
 
 describe('Entity: User', () => {
-  it('correctly set name, age, and birthday via ctor', () => {
-    const user = new User({
+  let user
+  beforeEach(() => {
+    user = new User({
       name: 'adrian',
       age: 17,
       birthday: new Date(1991, 0, 29),
     })
+  })
+  it('correctly set name, age, and birthday via ctor', () => {
     expect(user.name).toBe('adrian')
     expect(user.age).toBe(17)
     expect(user.birthday).toEqual(expect.any(Date))
+
+    user.name = 'abang jago'
   })
 
   it('correctly update name', () => {
-    const user = new User({
-      name: 'adrian',
-      age: 17,
-    })
     user.setName('padmodihardjo')
     expect(user.name).toBe('padmodihardjo')
   })
 
   it('correctly update age', () => {
-    const user = new User({
-      name: 'adrian',
-      age: 17,
-    })
     user.setAge(30)
     expect(user.age).toBe(30)
   })
