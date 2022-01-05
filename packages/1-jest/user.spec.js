71307905447d212c693641928f544b3f875c8506
adrianpdm <adrian.padmodihardjo@gmail.com>
2022-01-06 02:31:33 +0700

    test: add test suite for User's birthday ctor

--- packages/1-jest/user.spec.js
+++ packages/1-jest/user.spec.js
@@ -1,13 +1,15 @@
 const { User } = require('./user')
 
 describe('Entity: User', () => {
-  it('correctly set name and age via ctor', () => {
+  it('correctly set name, age, and birthday via ctor', () => {
     const user = new User({
       name: 'adrian',
       age: 17,
+      birthday: new Date(1991, 0, 29),
     })
     expect(user.name).toBe('adrian')
     expect(user.age).toBe(17)
+    expect(user.birthday).toEqual(expect.any(Date))
   })
 
   it('correctly update name', () => {
